28e9b66a0da33793d2e6628a33cacf8a299d5a80
Xenia Timoshenko <96055384+Xentention@users.noreply.github.com>
2022-11-01 18:27:49 +0300

    Add files via upload

---
+++ Консольная игра с мельницей/LocationAndFigure.cpp
@@ -0,0 +1,159 @@
+/**************************************************************************
+*                                ЛР4 ООП                                  *
+*-------------------------------------------------------------------------*
+*                                                                         *
+* Project Name  : ЛР4. ООП. Мельница. 									  *
+* Project Type  : Win32 Console application                               *
+* File Name     : LocationAndFigure.cpp	                                  *
+* Language      : Visual C++           MS VS 2022                         *
+* Programmer(s) : К.В.Тимошенко, М3О-211Б-20		                      *
+* Modified By   :														  *
+* Created       : 28 / 03 / 2022                                          *
+* Last Revision : 01 / 05 / 2022                                          *
+* Comment(s)    : Методы классов Location и Point			              *
+*																		  *
+**************************************************************************/
+
+#include "Utilities.h"   //Заголовочный файл с вспомогательными объявлениями
+#include "LocationAndFigure.h"
+
+extern HDC hdc;     //объявим  контекст устройства
+
+//прототипы вспомогательных для Hide() функций
+void HideSky(int X, int Y, int Width, int Height);
+void HideLighttoDarkSky(int X, int Y, int Width, int Height);
+void HideCrops(int X, int Y, int Width, int Height);
+
+/*----------------------------------------*/
+/*        МЕТОДЫ КЛАССА Location          */
+/*----------------------------------------*/
+
+//------------------------------------------------
+//конструктор
+Location::Location(int InitX, int InitY) {
+	X = InitX;
+	Y = InitY;
+
+};	//end Location::Location()
+
+//------------------------------------------------
+ //получить X координату точки
+int Location::GetX() {	return X;	};	
+
+//------------------------------------------------
+ //получить Y координату точки
+int Location::GetY() {	return Y;	};
+
+//------------------------------------------------
+//установить X координату точки
+void Location::SetX(int NewX) { X = NewX; };
+
+//------------------------------------------------
+//установить Y координату точки
+void Location::SetY(int NewY) { Y = NewY; };
+
+/*----------------------------------------*/
+/*        МЕТОДЫ КЛАССА Figure             */
+/*----------------------------------------*/
+
+ //--------------------------------------------------------------
+//конструктор
+Figure::Figure(int InitX, int InitY) : Location(InitX, InitY) {
+	Visible = false;
+}	//end Figure::Figure()
+
+ //--------------------------------------------------------------
+//Скрыть фигуру
+void Figure::Hide() {
+	if (!Visible) return;
+
+	Visible = false;					//Больше не видна
+	int Height = ReturnMaxHeight();		//Узнаем высоту фигуры
+	int Width = ReturnMaxWidth();		//Узнаем ширину фигуры
+
+	if (WheatLevel > GetY() - Height)				//Если фигура выше горизонта
+		HideSky(GetX(), GetY(), Width, Height);		//Закрашиваем небо
+
+	if (WheatLevel <= GetY() + Height)				//Если фигура ниже горизонта
+		HideCrops(GetX(), GetY(), Width, Height);	//Закрашиваем поле
+
+}	//end Figure::Hide()
+
+//--------------------------------------------------------------
+//переместить фигуру
+void Figure::MoveTo(int NewX, int NewY)	{
+	Hide();		//сделать фигуру невидимой
+	SetX(NewX);	//поменять ее координаты
+	SetY(NewY);
+	Show();		//показать фигуру на новом месте
+
+}	//end Figure::MoveTo()
+
+/*------------------------------------*/
+/* Вспомогательные функции для Hide() */
+/*------------------------------------*/
+
+//--------------------------------------------------------------
+//Закрасить небо
+void HideSky(int X, int Y, int Width, int Height) {
+	HBRUSH brush = CreateSolidBrush(SkyColor);		//ручка цвета неба
+	HPEN pen = CreatePen(PS_SOLID, 1, SkyColor);	//кисть цвета неба
+	SelectObject(hdc, brush);	//выбираем заданную кисть
+	SelectObject(hdc, pen);		//выбираем заданную ручку
+
+	//Закрашиваем фигуру
+	Rectangle(hdc, X - Width, Y - Height, X + Width, Y + Height);
+	DeleteObject(brush);	//Удаляем больше ненужную кисть
+	DeleteObject(pen);		//и ручку
+
+	HideLighttoDarkSky(X, Y, Width, Height);	//Закрашиваем светлую часть неба
+
+}	//end HideSky()
+
+//--------------------------------------------------------------
+//Закрасить светлое небо
+void HideLighttoDarkSky(int X, int Y, int Width, int Height) {
+	HBRUSH brush = CreateSolidBrush(LightSkyColor);		//кисть цвета светлого неба
+	HPEN pen = CreatePen(PS_SOLID, 1, LightSkyColor);	//ручка цвета светлого неба
+	SelectObject(hdc, brush);	//выбираем заданную кисть
+	SelectObject(hdc, pen);		//выбираем заданную ручку
+
+	//Закрашиваем светлое небо
+	if (LightSkyLevel <= Y + Height)
+		Rectangle(hdc, X - Width, LightSkyLevel, X + Width, Y + Height);
+
+	//Обновляем градиент на небе
+	if (LightSkyLevel > Y - Height) {
+		Rectangle(hdc, X - Width, LightSkyLevel - 3, X + Width, LightSkyLevel - 15);
+		Rectangle(hdc, X - Width, LightSkyLevel - 19, X + Width, LightSkyLevel - 29);
+		Rectangle(hdc, X - Width, LightSkyLevel - 34, X + Width, LightSkyLevel - 39);
+		Rectangle(hdc, X - Width, LightSkyLevel - 45, X + Width, LightSkyLevel - 50);
+		Rectangle(hdc, X - Width, LightSkyLevel - 60, X + Width, LightSkyLevel - 64);
+		Rectangle(hdc, X - Width, LightSkyLevel - 77, X + Width, LightSkyLevel - 80);
+	}
+
+	DeleteObject(brush);	//Удаляем больше ненужную кисть
+	DeleteObject(pen);		//и ручку
+
+}	//end HideLighttoDarkSky()
+
+//--------------------------------------------------------------
+//Закрасить поле
+void HideCrops(int X, int Y, int Width, int Height) {
+	SetBkMode(hdc, OPAQUE);			//фон кисти не прозрачный
+	SetBkColor(hdc, WheatColor);	//фон кисти цвета пшеницы
+	//Кисть в полоску цвета темной пшеницы
+	HBRUSH brush = CreateHatchBrush(HS_BDIAGONAL, DarkWheatColor);
+	//Ручка цвета пшеницы
+	HPEN pen = CreatePen(PS_SOLID, 1, WheatColor);
+	SelectObject(hdc, brush);	//выбираем заданную кисть
+	SelectObject(hdc, pen);		//выбираем заданную ручку
+
+	int UpperLevel = max(Y - Height, WheatLevel);
+	//Закрашиваем поле
+	Rectangle(hdc, X - Width, UpperLevel, X + Width, Y + Height);
+
+	DeleteObject(brush);	//Удаляем больше ненужную кисть
+	DeleteObject(pen);		//и ручку
+
+}	//end HideCrops()
---
+++ Консольная игра с мельницей/Utilities.h
@@ -0,0 +1,58 @@
+/**************************************************************************
+*                                ЛР4 ООП                                  *
+*-------------------------------------------------------------------------*
+*                                                                         *
+* Project Name  : ЛР4. ООП. Мельница. 									  *
+* Project Type  : Win32 Console application                               *
+* File Name     : Utilities.h	                                          *
+* Language      : Visual C++           MS VS 2022                         *
+* Programmer(s) : К.В.Тимошенко, М3О-211Б-20		                      *
+* Modified By   :														  *
+* Created       : 28 / 03 / 2022                                          *
+* Last Revision : 01 / 05 / 2022                                          *
+* Comment(s)    : Вспомогательные объявления 						      *
+*																		  *
+**************************************************************************/
+
+#pragma once
+
+#include <Windows.h>
+
+//макрос для определения кода нажатой клавиши
+#define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
+
+/*-----------------------  основные директивы  -------------------------*/
+#define FieldBottom 720						//Нижняя граница поля
+#define FieldRight 1280						//Правая граница поля
+#define WheatLevel 3*FieldBottom/5			//Уровень горизонта
+#define LightSkyLevel 3*WheatLevel/4		//Уровень светлого неба
+
+#define SkyColor RGB(129, 184, 202)			//Цвет неба
+#define LightSkyColor RGB(170, 229, 240)	//Цвет более светлого неба
+#define WheatColor RGB(255, 178, 78)		//Цвет пшеницы
+#define DarkWheatColor RGB(221, 115, 55)	//Цвет пшеницы темнее
+#define PondColor RGB(90, 188, 216)			//Цвет пруда
+
+#define MillColor RGB(174, 156, 153)		//Цвет каменной части мельницы
+#define WindshaftColor RGB(142,65, 61)		//Цвет деревянной части мельницы
+#define RoofColor RGB(171, 93, 93)			//Цвет крыши мельницы
+
+#define PondIsNearby 1						//Код того, что рядом пруд
+#define ServiceIsNearby 2					//Код того, что рядом рем. станция
+#define NothingNearby 0						//Код того, что ничего рядом нет
+
+#define indxMill 0							//Индекс обычной мельницы
+#define indxRMill 1							//Индекс мельницы с крышей
+#define indxWMill 2							//Индекс водяной мельницы
+#define indxWRMill 3						//Индекс водяной мельницы с крышей
+
+#define indxPond 0							//Индекс пруда
+#define indxService 1						//Индекс рем. станции
+
+const int NumofMills = 4;		//мельница, мельница с крышей, водяная мельница, водяная мельница с крышей
+const int NumofObstacles = 2;	//пруд, ремонтная станция
+const int NumofCollisions = 3;	//вне пруда, внутри пруда, столкнулись с рем. станцией
+const int TransitionMatrix[NumofMills][NumofCollisions] = { {indxMill, indxWMill, indxRMill},
+															{indxRMill, indxWRMill, indxMill},
+															{indxMill, indxWMill, indxWRMill},
+															{indxRMill, indxWRMill, indxWMill} };
